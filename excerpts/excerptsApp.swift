f1ba37da0ac79ba8f62d4c234f80d02a47567673
Richard Chien <stdrc@outlook.com>
2023-09-09 22:07:04 +0800

    Initial Commit

---
+++ excerpts/excerptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  excerptsApp.swift
+//  excerpts
+//
+//  Created by Richard on 2023/9/9.
+//
+
+import SwiftUI
+
+@main
+struct excerptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
