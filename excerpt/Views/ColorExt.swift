6cc2d338e468988329a3a4fab662a321d40847dc
Richard Chien <stdrc@outlook.com>
2023-09-14 00:59:21 +0800

    share card basically done
    Signed-off-by: Richard Chien <stdrc@outlook.com>

---
+++ excerpt/Views/ColorExt.swift
@@ -0,0 +1,50 @@
+//
+//  ColorExt.swift
+//  excerpt
+//
+//  Created by Richard on 2023/9/13.
+//
+
+import SwiftUI
+
+extension Color {
+    // from https://swiftuirecipes.com/blog/hex-color-in-swiftui
+    init?(_ hex: String) {
+        var str = hex
+        if str.hasPrefix("#") {
+            str.removeFirst()
+        }
+        if str.count == 3 {
+            str = String(repeating: str[str.startIndex], count: 2)
+                + String(repeating: str[str.index(str.startIndex, offsetBy: 1)], count: 2)
+                + String(repeating: str[str.index(str.startIndex, offsetBy: 2)], count: 2)
+        } else if !str.count.isMultiple(of: 2) || str.count > 8 {
+            return nil
+        }
+        guard let color = UInt64(str, radix: 16)
+        else {
+            return nil
+        }
+        if str.count == 2 {
+            let gray = Double(Int(color) & 0xFF) / 255
+            self.init(.sRGB, red: gray, green: gray, blue: gray, opacity: 1)
+        } else if str.count == 4 {
+            let gray = Double(Int(color >> 8) & 0x00FF) / 255
+            let alpha = Double(Int(color) & 0x00FF) / 255
+            self.init(.sRGB, red: gray, green: gray, blue: gray, opacity: alpha)
+        } else if str.count == 6 {
+            let red = Double(Int(color >> 16) & 0x0000FF) / 255
+            let green = Double(Int(color >> 8) & 0x0000FF) / 255
+            let blue = Double(Int(color) & 0x0000FF) / 255
+            self.init(.sRGB, red: red, green: green, blue: blue, opacity: 1)
+        } else if str.count == 8 {
+            let red = Double(Int(color >> 24) & 0x000000FF) / 255
+            let green = Double(Int(color >> 16) & 0x000000FF) / 255
+            let blue = Double(Int(color >> 8) & 0x000000FF) / 255
+            let alpha = Double(Int(color) & 0x000000FF) / 255
+            self.init(.sRGB, red: red, green: green, blue: blue, opacity: alpha)
+        } else {
+            return nil
+        }
+    }
+}
--- excerpt/Views/ShareView.swift
+++ excerpt/Views/ShareView.swift
@@ -7,6 +7,77 @@
 
 import SwiftUI
 
+func round(_ value: Double, toNearest: Double) -> Double {
+    return round(value / toNearest) * toNearest
+}
+
+struct Card: View {
+    var excerpt: Excerpt
+    var width: CGFloat
+
+    private let fontName = "SourceHanSerifSC-Regular"
+    private let fontSizeContent: CGFloat = 18
+    private let fontSizeFrom: CGFloat = 15
+    private let colorBackground = Color("F9F9FB")!
+    private let colorContent = Color("272220")!
+    private let colorFrom = Color("514A48")!
+    private let colorBorder = Color("D0CDCF")!
+
+    private let rectOuterPadding: CGFloat = 15
+
+    private var rectInnerWidth: CGFloat {
+        self.width - self.rectOuterPadding * 2
+    }
+
+    private var contentWidth: CGFloat {
+        round(self.rectInnerWidth - self.fontSizeContent * 4, toNearest: self.fontSizeContent)
+    }
+
+    private var contentVertOuterPadding: CGFloat {
+        (self.rectInnerWidth - self.contentWidth) / 2
+    }
+
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Text(self.excerpt.content)
+                        .font(.custom(self.fontName, size: self.fontSizeContent))
+                        .foregroundColor(self.colorContent)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, self.fontSizeContent)
+
+                    if !self.excerpt.author.isEmpty {
+                        Text("— \(self.excerpt.author)")
+                            .font(.custom(self.fontName, size: self.fontSizeFrom))
+                            .foregroundColor(self.colorFrom)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .multilineTextAlignment(.trailing)
+                    }
+                    if !self.excerpt.book.isEmpty {
+                        Text(self.excerpt.book)
+                            .font(.custom(self.fontName, size: self.fontSizeFrom))
+                            .foregroundColor(self.colorFrom)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                .padding([.leading, .trailing], self.contentVertOuterPadding)
+                .padding([.top, .bottom], self.contentVertOuterPadding * 1.6)
+                .border(self.colorBorder, width: 0.7)
+                .overlay {
+                    Rectangle()
+                        .fill(Color.clear)
+                        .border(self.colorBorder, width: 0.5)
+                        .padding(2)
+                }
+            }
+            .padding(self.rectOuterPadding)
+        }
+        .background(self.colorBackground)
+    }
+}
+
 struct ShareView: View {
     @Binding var isPresented: Bool
 
@@ -20,57 +91,11 @@ struct ShareView: View {
         GeometryReader { geometry in
             VStack(spacing: 0) {
                 ScrollView(.vertical, showsIndicators: false) {
-                    VStack {
-                        VStack(alignment: .trailing) {
-                            Text(self.excerpt.content)
-                                .font(.custom("SourceHanSerifSC-Regular", size: 18))
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.bottom, 10)
-
-                            if !self.excerpt.book.isEmpty {
-                                Text(self.excerpt.book)
-                                    .font(.custom("SourceHanSerifSC-Regular", size: 18))
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-                            if !self.excerpt.author.isEmpty {
-                                Text(self.excerpt.author)
-                                    .font(.custom("SourceHanSerifSC-Regular", size: 18))
-                                    .padding(.trailing, 2)
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-
-//                            HStack(spacing: 0) {
-//                                Spacer()
-//
-//                                Text("——")
-//                                    .font(.custom("SourceHanSerifSC-Regular", size: 18))
-//                                    .padding(.trailing, 6)
-//
-//                                if !self.excerpt.author.isEmpty {
-//                                    Text(self.excerpt.author)
-//                                        .font(.custom("SourceHanSerifSC-Regular", size: 18))
-//                                        .padding(.trailing, 2)
-//                                }
-//                                if !self.excerpt.book.isEmpty {
-//                                    Text("《\(self.excerpt.book)》")
-//                                        .font(.custom("SourceHanSerifSC-Regular", size: 18))
-//                                }
-//                            }
-//                            .frame(maxWidth: .infinity)
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding(18)
-                    }
-                    .background(Color.white)
-                    .foregroundStyle(.black)
-                    .padding([.leading, .trailing], 15)
-                    .frame(width: geometry.size.width)
-                    .frame(minHeight: geometry.size.height)
-                    .onTapGesture {
-                        self.dismiss()
-                    }
+                    Card(excerpt: self.excerpt, width: geometry.size.width - 15 * 2)
+                        .padding(15)
+                        .frame(width: geometry.size.width)
+                        .frame(minHeight: geometry.size.height)
                 }
-//                .background(Color.blue)
                 .onTapGesture {
                     self.dismiss()
                 }
@@ -102,7 +127,7 @@ struct ShareView: View {
 }
 
 #Preview("Share Long") {
-    MainView(Excerpt(id: UUID(), content: excerpts[0].content + "\n" + excerpts[0].content, book: "这是一本名字超长的书：甚至还有副标题", author: "名字超长的作者·甚至还有 Last Name·以及更多"), sharing: true)
+    MainView(Excerpt(id: UUID(), content: excerpts[0].content + "\n" + excerpts[0].content + "\n" + excerpts[0].content, book: "这是一本名字超长的书：甚至还有副标题", author: "名字超长的作者·甚至还有 Last Name·以及更多"), sharing: true)
         .environment(\.locale, .init(identifier: "zh-Hans"))
 }
 
