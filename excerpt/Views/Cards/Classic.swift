de852823e63d621b4696ef02bff2ffb9ed712eeb
Richard Chien <stdrc@outlook.com>
2023-09-17 18:10:14 +0800

    refactor classic card style
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- excerpt/Views/Cards/Classic.swift
+++ excerpt/Views/Cards/Classic.swift
@@ -7,19 +7,51 @@
 
 import SwiftUI
 
+private enum C {
+    static let colorBackground = Color("F9F9FB")!
+    static let colorBorder = Color("D0CDCF")!
+
+    static let fontSizeContent: CGFloat = 17
+    static let colorContent = Color("272220")!
+    static let fontSizeFrom: CGFloat = 14
+    static let colorFrom = Color("514A48")!
+    static let fontSizeWatermark: CGFloat = 10
+    static let colorWatermark = Color("D0CDCF")!
+}
+
+private extension Text {
+    func withNiceFont(_ size: CGFloat) -> Text {
+        self.font(.custom("SourceHanSerifSC-Regular", size: size))
+    }
+
+    func withSystemFont(_ size: CGFloat) -> Text {
+        self.font(.system(size: size))
+    }
+
+    func inContentSection() -> some View {
+        self.withNiceFont(C.fontSizeContent)
+            .foregroundColor(C.colorContent)
+            .multilineTextAlignment(.leading)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .lineSpacing(C.fontSizeContent * 0.2)
+    }
+
+    func inFromSection() -> some View {
+        self.withNiceFont(C.fontSizeFrom)
+            .foregroundColor(C.colorFrom)
+            .multilineTextAlignment(.trailing)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+    }
+
+    func inWatermarkSection() -> some View {
+        self.withSystemFont(C.fontSizeWatermark)
+            .foregroundColor(C.colorWatermark)
+    }
+}
+
 struct ClassicCard: View {
     var excerpt: Excerpt
     var width: CGFloat
-
-    private let fontName = "SourceHanSerifSC-Regular"
-    private let fontSizeContent: CGFloat = 17
-    private let fontSizeFrom: CGFloat = 14
-    private let fontSizeWatermark: CGFloat = 10
-    private let colorBackground = Color("F9F9FB")!
-    private let colorContent = Color("272220")!
-    private let colorFrom = Color("514A48")!
-    private let colorBorder = Color("D0CDCF")!
-    private let colorWatermark = Color("D0CDCF")!
 
     private let rectOuterPadding: CGFloat = 15
 
@@ -28,7 +60,7 @@ struct ClassicCard: View {
     }
 
     private var contentWidth: CGFloat {
-        round(self.rectInnerWidth - self.fontSizeContent * 3, toNearest: self.fontSizeContent)
+        round(self.rectInnerWidth - C.fontSizeContent * 3, toNearest: C.fontSizeContent)
     }
 
     private var contentVertOuterPadding: CGFloat {
@@ -43,49 +75,29 @@ struct ClassicCard: View {
         VStack(alignment: .leading) {
             VStack {
                 VStack(spacing: self.contentFromSpacing) {
-                    VStack(spacing: self.fontSizeContent) {
+                    VStack(spacing: C.fontSizeContent) {
                         ForEach(Array(self.excerpt.contentLinesTrimmed().enumerated()), id: \.offset) { _, paragraph in
                             if !paragraph.isEmpty {
-                                Text(paragraph)
-                                    .font(.custom(self.fontName, size: self.fontSizeContent))
-                                    .foregroundColor(self.colorContent)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .lineSpacing(self.fontSizeContent * 0.2)
+                                Text(paragraph).inContentSection()
                             }
                         }
                     }
 
                     if !(self.excerpt.titleTrimmed.isEmpty && self.excerpt.authorTrimmed.isEmpty) {
-                        VStack(spacing: self.fontSizeFrom * 0.2) {
+                        VStack(spacing: C.fontSizeFrom * 0.2) {
                             if self.excerpt.type != .lyrics {
                                 if !self.excerpt.authorTrimmed.isEmpty {
-                                    Text("CARD_VIEW_AUTHOR_TPLT \(self.excerpt.authorTrimmed)")
-                                        .font(.custom(self.fontName, size: self.fontSizeFrom))
-                                        .foregroundColor(self.colorFrom)
-                                        .frame(maxWidth: .infinity, alignment: .trailing)
-                                        .multilineTextAlignment(.trailing)
+                                    Text("CARD_VIEW_AUTHOR_TPLT \(self.excerpt.authorTrimmed)").inFromSection()
                                 }
                                 if !self.excerpt.titleTrimmed.isEmpty {
-                                    Text(self.excerpt.titleTrimmed)
-                                        .font(.custom(self.fontName, size: self.fontSizeFrom))
-                                        .foregroundColor(self.colorFrom)
-                                        .frame(maxWidth: .infinity, alignment: .trailing)
-                                        .multilineTextAlignment(.trailing)
+                                    Text(self.excerpt.titleTrimmed).inFromSection()
                                 }
                             } else {
                                 if !self.excerpt.titleTrimmed.isEmpty {
-                                    Text(self.excerpt.titleTrimmed)
-                                        .font(.custom(self.fontName, size: self.fontSizeFrom))
-                                        .foregroundColor(self.colorFrom)
-                                        .frame(maxWidth: .infinity, alignment: .trailing)
-                                        .multilineTextAlignment(.trailing)
+                                    Text(self.excerpt.titleTrimmed).inFromSection()
                                 }
                                 if !self.excerpt.authorTrimmed.isEmpty {
-                                    Text(self.excerpt.authorTrimmed)
-                                        .font(.custom(self.fontName, size: self.fontSizeFrom))
-                                        .foregroundColor(self.colorFrom)
-                                        .frame(maxWidth: .infinity, alignment: .trailing)
-                                        .multilineTextAlignment(.trailing)
+                                    Text(self.excerpt.authorTrimmed).inFromSection()
                                 }
                             }
                         }
@@ -93,11 +105,10 @@ struct ClassicCard: View {
                 }
                 .padding([.leading, .trailing], self.contentVertOuterPadding)
                 .padding([.top, .bottom], self.contentVertOuterPadding * 1.6)
-                .border(self.colorBorder, width: 0.7)
+                .border(C.colorBorder, width: 0.7)
                 .overlay {
-                    Rectangle()
-                        .fill(Color.clear)
-                        .border(self.colorBorder, width: 0.5)
+                    Color.clear
+                        .border(C.colorBorder, width: 0.5)
                         .padding(2)
                 }
             }
@@ -105,20 +116,13 @@ struct ClassicCard: View {
             .padding(.bottom, 3)
 
             HStack(spacing: 2) {
-                Text("CARD_VIEW_SHARED_VIA_BEGIN")
-                    .font(.system(size: self.fontSizeWatermark))
-                    .foregroundColor(self.colorWatermark)
-                Text("C_APP_NAME")
-                    .font(.system(size: self.fontSizeWatermark))
-                    .foregroundColor(self.colorWatermark)
-                    .bold()
-                Text("CARD_VIEW_SHARED_VIA_END")
-                    .font(.system(size: self.fontSizeWatermark))
-                    .foregroundColor(self.colorWatermark)
+                Text("CARD_VIEW_SHARED_VIA_BEGIN").inWatermarkSection()
+                Text("C_APP_NAME").bold().inWatermarkSection()
+                Text("CARD_VIEW_SHARED_VIA_END").inWatermarkSection()
             }
             .padding([.leading, .bottom, .trailing], self.rectOuterPadding)
         }
-        .background(self.colorBackground)
+        .background(C.colorBackground)
     }
 }
 
