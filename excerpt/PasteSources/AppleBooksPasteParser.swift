e413abb10b456bff033a834fb65b370fa5e2c910
Richard Chien <stdrc@outlook.com>
2023-12-21 20:03:43 +0800

    support english excerpt template
    Signed-off-by: Richard Chien <stdrc@outlook.com>

--- excerpt/PasteSources/AppleBooksPasteParser.swift
+++ excerpt/PasteSources/AppleBooksPasteParser.swift
@@ -7,18 +7,21 @@
 
 import Foundation
 
-private let appleBooksExcerptTplt = /^“([\S\s]*)”\s*摘录来自\n([^\n]+)\n([^\n]+)(?:\n[^\n]+)?\n此材料(?:可能)?受版权保护。$/
-// TODO: english
+private let appleBooksExcerptTplts: [Regex] = [
+    /^“([\S\s]*)”\s*摘录来自\n([^\n]+)\n([^\n]+)(?:\n[^\n]+)?\n[^\n]*材料[^\n]*版权保护。$/,
+    /^“([\S\s]*)”\s*Excerpt From\n([^\n]+)\n([^\n]+)(?:\n[^\n]+)?\n[^\n]*material[^\n]*protected by copyright\.$/,
+]
 
 struct AppleBooksPasteParser: PasteParser {
     func parse(_ content: String, excerpt: inout Excerpt) -> Bool {
-        if let match = content.wholeMatch(of: appleBooksExcerptTplt) {
-            excerpt.content = String(match.1)
-            excerpt.title = String(match.2)
-            excerpt.author = String(match.3)
-            return true
-        } else {
-            return false
+        for tplt in appleBooksExcerptTplts {
+            if let match = content.wholeMatch(of: tplt) {
+                excerpt.content = String(match.1)
+                excerpt.title = String(match.2)
+                excerpt.author = String(match.3)
+                return true
+            }
         }
+        return false
     }
 }
